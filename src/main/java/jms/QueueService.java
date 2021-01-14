c0134cf9287d6c34dbcc62663e51e46181b9f983
Ol Gen <oggen18@gmail.com>
2021-01-14 14:48:43 +0300

    PutIfAbsent, get().add() in "post" method of QueueService.

--- src/main/java/jms/QueueService.java
+++ src/main/java/jms/QueueService.java
@@ -1,6 +1,5 @@
 package jms;
 
-import java.util.Collections;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,18 +13,17 @@ public class QueueService implements Service {
         java.util.Queue<String> queue = messages.get(nameOfQueue);
         if (queue == null) {
             connection.write("Such queue doesn't exist");
+            System.out.println(Thread.currentThread().getName() + " Such queue doesn't exist");
         } else {
             String response = queue.isEmpty() ? "The queue is empty" : queue.poll();
+            System.out.println(Thread.currentThread().getName() + " responses: " + response);
             connection.write(response);
         }
     }
 
     @Override
     public void post(Connection connection, String nameOfQueue, String text) {
-        java.util.Queue<String> tmpQueue = messages.putIfAbsent(
-                nameOfQueue, new ConcurrentLinkedQueue<>(Collections.singleton(text)));
-        if (tmpQueue != null) {
-            tmpQueue.add(text);
-        }
+        messages.putIfAbsent(nameOfQueue, new ConcurrentLinkedQueue<>());
+        messages.get(nameOfQueue).add(text);
     }
 }
